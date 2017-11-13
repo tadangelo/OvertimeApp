4bee720ee3f4266305657c51125dedc980e36bde
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-12 19:49:25 -0600

    Rendered list of posts via behavior driven development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 @user = User.create(email: "test1@test.com", password: "test1test", password_confirmation: "test1test", first_name: "John", last_name: "Smith")
 
+puts "1 user created"
+
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
