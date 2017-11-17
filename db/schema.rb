09b8d283b81b016a1f89020297bf822d80774ead
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-17 13:44:25 -0600

    Implemented enum for status into posts for approval workflow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109210818) do
+ActiveRecord::Schema.define(version: 20171117193157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,8 @@ ActiveRecord::Schema.define(version: 20171109210818) do
     t.text "rationale"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
+    t.integer "user_id"
+    t.integer "status", default: 0
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,8 +42,6 @@ ActiveRecord::Schema.define(version: 20171109210818) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-    add_foreign_key "posts", "users"
   end
 
 end
