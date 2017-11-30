82995e1c2fc3c9d9858d25ab68741007da3327bf
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-30 12:11:39 -0600

    Updated seeds test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,16 +16,6 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 admin user created"
 
-
-100.times do |post|
-  Post.create!(date: Date.today,
-                rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                user_id: @user.id,
-                overtime_request: 2.5)
-end
-
-puts "100 posts have been created"
-
 AuditLog.create!(user_id: @user.id,
                     status: 0,
                     start_date: (Date.today - 6.days))
@@ -37,3 +27,12 @@ AuditLog.create!(user_id: @user.id,
                     start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
+
+100.times do |post|
+  Post.create!(date: Date.today,
+                rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                user_id: @user.id,
+                overtime_request: 2.5)
+end
+
+puts "100 posts have been created"
