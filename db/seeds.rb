a66d6f23c4a9655f6de523c38359106bef87ddd2
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-01 00:30:32 -0600

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,28 @@
-@user = User.create(email: "test1@test.com",
+@Employee = Employee.create(email: "test1@test.com",
                     password: "test1test",
                     password_confirmation: "test1test",
                     first_name: "John",
                     last_name: "Smith",
                     phone: "3378573118")
 
-puts "1 user created"
+puts "1 Employee created"
 
-AdminUser.create(email: "admin@test.com",
+AdminEmployee.create(email: "admin@test.com",
                   password: "admintest",
                   password_confirmation: "admintest",
                   first_name: "Admin",
-                  last_name: "User",
+                  last_name: "Employee",
                   phone: "3378573118")
 
-puts "1 admin user created"
+puts "1 admin Employee created"
 
-AuditLog.create!(user_id: @user.id,
+AuditLog.create!(user_id: @Employee.id,
                     status: 0,
                     start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id,
+AuditLog.create!(user_id: @Employee.id,
                     status: 0,
                     start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id,
+AuditLog.create!(user_id: @Employee.id,
                     status: 0,
                     start_date: (Date.today - 20.days))
 
@@ -31,7 +31,7 @@ puts "3 audit logs have been created"
 100.times do |post|
   Post.create!(date: Date.today,
                 rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                user_id: @user.id,
+                user_id: @Employee.id,
                 overtime_request: 2.5)
 end
 
