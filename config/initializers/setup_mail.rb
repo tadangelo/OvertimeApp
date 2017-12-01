9ac74c34f0d093607ff277c3765928c5cafc141e
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-12-01 15:25:03 -0600

    Updated sendgrid domain

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'https://dangelo-overtime.herokuapp.com/',
+  :domain => 'https://dangelo-overtime.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
